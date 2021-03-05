72868e26cbf5b142ba9afb8f120a812329e08679
dotoryeee <bayer@kakao.com>
2021-03-06 02:50:15 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_hvh0CCOBb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6r2916tj35rqh7uor0290aa455', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6e8rj34hhv2eitpdf7mdqau873', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
