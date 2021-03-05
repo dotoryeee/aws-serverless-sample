d0270d742cfc69850be385828ddac24754c257f1
dotoryeee <bayer@kakao.com>
2021-03-06 01:22:42 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var _config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_27iZnXMdV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6rv65lbbim1t4576vj78k1t9iu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2', // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
+    }
+};
+    
